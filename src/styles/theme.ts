a2e2eed8ae9be2fb2eb05228ab04db75ab2bc9bb
Wagner Cardoso <wagnerdev01@gmail.com>
2021-11-09 22:50:28 -0300

    feat() Add initial app

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    background: '#ffffff',
+    textPrimary: '#121214',
+    textSecondary: '#f9f9f9',
+    primary: '#28AE5E',
+    secondary: '#EFF321',
+  },
+};
+
+export default theme;
