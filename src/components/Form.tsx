3851ec766d3372acf7e573d506651b826b3dcea8
Wagner Cardoso <wagnerdev01@gmail.com>
2021-11-11 00:10:55 -0300

    feat(components) Add new help form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import styles from '../styles/components/Form';
+
+const {
+  FormContainer,
+  Input,
+  InputContainer,
+  SubmitButton,
+} = styles;
+
+const Form: React.FC = () => {
+  const [name, setName] = useState('');
+
+  const onChangeName = (event) => setName(event.target.value);
+
+  const onSubmit = (event: any) => {
+    event.preventDefault();
+  };
+
+  return (
+    <FormContainer>
+      <InputContainer>
+        <label htmlFor="name">Nome:</label>
+        <Input
+          type="text"
+          name="name"
+          value={name}
+          onChange={onChangeName}
+        />
+      </InputContainer>
+      <InputContainer>
+        <label htmlFor="email">Email:</label>
+        <Input
+          type="email"
+          name="email"
+          value={name}
+          onChange={onChangeName}
+        />
+      </InputContainer>
+      <InputContainer>
+        <label htmlFor="telephone">Telefone:</label>
+        <Input
+          type="tel"
+          name="telephone"
+          value={name}
+          onChange={onChangeName}
+        />
+      </InputContainer>
+      <InputContainer>
+        <label htmlFor="address">Endereço:</label>
+        <Input
+          type="text"
+          name="address"
+          value={name}
+          onChange={onChangeName}
+        />
+      </InputContainer>
+      <InputContainer>
+        <label htmlFor="number">Número:</label>
+        <Input
+          type="text"
+          name="number"
+          value={name}
+          onChange={onChangeName}
+        />
+      </InputContainer>
+      <InputContainer>
+        <label htmlFor="neighborhood">Bairro:</label>
+        <Input
+          name="neighborhood"
+          value={name}
+          onChange={onChangeName}
+        />
+      </InputContainer>
+      <InputContainer>
+        <label htmlFor="complement">Complemento:</label>
+        <Input
+          name="complement"
+          value={name}
+          onChange={onChangeName}
+        />
+      </InputContainer>
+      <InputContainer>
+        <label htmlFor="description">Descrição:</label>
+        <Input
+          name="description"
+          value={name}
+          onChange={onChangeName}
+        />
+      </InputContainer>
+      <SubmitButton
+        type="submit"
+        value="Cadastrar"
+        onClick={onSubmit}
+      />
+    </FormContainer>
+  );
+};
+
+export default Form;
